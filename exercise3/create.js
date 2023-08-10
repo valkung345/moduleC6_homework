2c2542bd0ab8bc520e54833b9a5b992ee609c135
valkung345 <121906475+valkung345@users.noreply.github.com>
2023-08-10 16:32:35 +0500

    Update create.js

--- exercise3/create.js
+++ exercise3/create.js
@@ -1,46 +1,69 @@
-const wsUri = "wss://ws.postman-echo.com/raw";
+const wsUri = " wss://echo-ws-service.herokuapp.com";
 
-const output = document.getElementById("output");
-const btnOpen = document.querySelector('.j-btn-open');
-const btnClose = document.querySelector('.j-btn-close');
-const btnSend = document.querySelector('.j-btn-send');
+const input = document.querySelector('.input');
+const btnMess = document.querySelector('.btn-mess');
+const btnGeo = document.querySelector('.btn-geo');
+const userMessages = document.querySelector('.user-messages');
+const serverMessages = document.querySelector('.server-messages');
+const wrapperChat =  document.querySelector('.wrapper-chat');
 
-let websocket;
+//Выводит сообщения
+function writeToScreen(message, position='flex-end') {
+	let element = `
+        <p class='messages' style='align-self: ${position}'>
+            ${message}
+        </p>
+    `;
+	userMessages.innerHTML += element;
+	wrapperChat.scrollTop = wrapperChat.scrollHeight;
+  }
 
-function writeToScreen(message) {
-  let pre = document.createElement("p");
-  pre.style.wordWrap = "break-word";
-  pre.innerHTML = message;
-  output.appendChild(pre);
-}
+//Объект соединения
+ let websocket = new WebSocket(wsUri); 
+	websocket.onopen = function(evt) {
+		console.log("CONNECTED");
+	};
+	websocket.onmessage = function(evt) {
+		writeToScreen(`ответ сервера: ${evt.data}`, 'flex-start');
+	};
+	websocket.onerror = function(evt) {
+		writeToScreen(`server: ${evt.data}`, 'flex-start');
+	};
+  
+  //отправка сообщения
+  btnMess.addEventListener('click', () => {
+	let message = input.value;
+	websocket.send(message);
+	writeToScreen(`Вы: ${message}`);
+	input.value = ''
 
-btnOpen.addEventListener('click', () => {
-  websocket = new WebSocket(wsUri);
-  websocket.onopen = function(evt) {
-    writeToScreen("CONNECTED");
+  });
+
+  
+  //гео-локация.
+  // Функция,  об ошибке
+const error = () => {
+	let textErr0r = 'Невозможно получить ваше местоположение';
+	writeToScreen(textErr0r);
   };
-  websocket.onclose = function(evt) {
-    writeToScreen("DISCONNECTED");
+  
+  // Функция, срабатывающая при успешном получении геолокации
+  const success = (position) => {
+	let latitude  = position.coords.latitude;
+	let longitude = position.coords.longitude;
+	let geoLink = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+	writeToScreen(`<a  href='${geoLink}' target='_blank'>Ваша гео-локация</a>`);
   };
-  websocket.onmessage = function(evt) {
-    writeToScreen(
-      '<span style="color: blue;">RESPONSE: ' + evt.data+'</span>'
-    );
-  };
-  websocket.onerror = function(evt) {
-    writeToScreen(
-      '<span style="color: red;">ERROR:</span> ' + evt.data
-    );
-  };
-});
+  
+  btnGeo.addEventListener('click', () => {
+	if (!navigator.geolocation) {
+	  console.log('Geolocation не поддерживается вашим браузером');
+	} else {
+	  navigator.geolocation.getCurrentPosition(success, error);
+	}
+  });
 
-btnClose.addEventListener('click', () => {
-  websocket.close();
-  websocket = null;
-});
-
-btnSend.addEventListener('click', () => {
-  const message = 'Test message';
-  writeToScreen("SENT: " + message);
-  websocket.send(message);
-});
+  //удаляем сообщения
+  serverMessages.addEventListener('click', () => {
+	userMessages.innerHTML = " ";
+  });
